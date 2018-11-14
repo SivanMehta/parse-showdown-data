de2c519fca5fafb83c4afae3267c615cf1b00191
Sivan Mehta <Sivanmehta2@gmail.com>
2018-11-13 20:41:49 -0800

    working file that is parseable

--- parse.js
+++ parse.js
@@ -18,29 +18,50 @@ function readLines(lines) {
   const data = {};
   let currentPoke = '';
   let currentSection = '';
-  let currentlyMeta = false;
+  let delimitersInARow = 0;
 
   lines.forEach(line => {
     if (line.match(delimiter)) { // cut current section
-      currentPoke = '';
-      currentSection = '';
+      delimitersInARow ++;
+      if(delimitersInARow == 2) {
+        currentPoke = '';
+        currentSection = '';
+        delimitersInARow = 0;
+      }
+    } else {
+      delimitersInARow = 0;
     }
 
     const section = sections.find(s => line.match(s));
+    let inASection = false;
     if (section) {
       currentSection = section;
+      inASection = true;
     }
 
     if (currentPoke === '' && !currentSection) {
       const short = line.match(/ \| ([A-Za-z])+.+([A-Za-z])+/g);
       currentPoke = short ? short[0].substr(3, short[0].length) : '';
+      data[currentPoke] = {};
     }
 
-    console.log(line, currentSection, currentPoke);
-  })
+    if(inASection) {
+      data[currentPoke][currentSection] = [];
+    } else if (currentSection.length > 0 && currentPoke.length > 0 && !inASection) {
+      const isData = line.split("|")[1];
 
+      const parsed = isData ? isData.trim() : null;
+
+      if(parsed) {
+        data[currentPoke][currentSection].push(parsed);
+      }
+
+    }
+  });
+
+  console.log(JSON.stringify(data, null, 2));
 }
 
-fs.readFile('./data.txt', { encoding: 'utf-8' }, (err, data) => {
+fs.readFile('./raw/gen7ubers-1760.txt', { encoding: 'utf-8' }, (err, data) => {
   readLines(data.split("\n"))
 });
